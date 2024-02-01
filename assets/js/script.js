087917aaa580db36783b061a74bdc4d58f4b3c1a
Mohamed Suhail <suhailmhd070@gmail.com>
2024-02-01 14:40:28 +0530

    alignment fixed in mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,23 @@ const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 // navbar variables
 const navbar = document.querySelector("[data-navbar]");
-const navbarOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbarCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navbarOpenBtn = document.querySelector(".nav-open-btn");
+const navbarCloseBtn = document.querySelector(".nav-close-btn");
 
-navbarOpenBtn.addEventListener("click", function () {
-  elemToggleFunc(navbar);
+// navbarOpenBtn.addEventListener("click", function () {
+//   elemToggleFunc(navbar);
+// });
+
+// navbarCloseBtn.addEventListener("click", function () {
+//   elemToggleFunc.remove(navbar);
+// });
+
+navbarOpenBtn.addEventListener('click', () => {
+  navbar.classList.toggle('active');
 });
 
-navbarCloseBtn.addEventListener("click", function () {
-  elemToggleFunc(navbar);
+navbarCloseBtn.addEventListener('click', () => {
+  navbar.classList.toggle('active');
 });
 
 
